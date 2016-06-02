27500ee3b0fe67bc30a757f8f6dcd5a35a4b86c6
Bojie Li <bojieli@gmail.com>
2016-06-03 02:06:05 +0800

    removed unused create index

--- create-index.sh
+++ create-index.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-if [ -z "$1" ] || [ -z "$2" ]; then
-	echo "Usage: $0 <database-name> <index-name>"
+if [ -z "$1" ]; then
+	echo "Usage: $0 <index-name>"
 	exit 1
 fi
 
-DBNAME=$1
-INDEXNAME=$2
+INDEXNAME=$1
 curl -X PUT "http://127.0.0.1:9200/$INDEXNAME?pretty=true" --data @index-httppost.conf
