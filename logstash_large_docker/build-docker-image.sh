a88acdf0f8aed85dc28e730e006244e47959ae2b
Bojie Li <bojieli@gmail.com>
2016-06-25 04:28:12 +0800

    logstash with large mem support

---
+++ logstash_large_docker/build-docker-image.sh
@@ -0,0 +1,1 @@
+docker build -t logstash-large:latest .
---
+++ logstash_large_docker/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+export LS_HEAP_SIZE="16384m"
+
+# Add logstash as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- logstash "$@"
+fi
+
+# Run as user "logstash" if the command is "logstash"
+if [ "$1" = 'logstash' ]; then
+	set -- gosu logstash "$@"
+fi
+
+exec "$@"
