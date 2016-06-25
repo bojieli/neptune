90a8ce51a8f6184b510d66812669f103d96b1e97
Bojie Li <bojieli@gmail.com>
2016-06-25 19:28:37 +0800

    save sync sequence to .couchdb_seq (similar to logstash)

--- sync.js
+++ sync.js
@@ -2,9 +2,9 @@ var request = require('request')
   , JSONStream = require('JSONStream')
   , es = require('event-stream')
   , elasticsearch = require('elasticsearch')
+  , fs = require('fs')
 
 var db = 'http://localhost:5984/moldb2'
-var since_number = 0
 var es_client = new elasticsearch.Client({host: 'localhost:9200'})
 var index_name = "molecule3"
 var max_in_flight_reqs = 100
@@ -12,12 +12,21 @@ var curr_in_flight_reqs = 0
 var is_bulk_in_flight = false
 var bulk_buffer = []
 
+var since_number = 0
+try {
+	since_number = fs.readFileSync('.couchdb_seq')
+} catch(e) {
+	console.log(e)
+}
+console.log("Starting syncing from " + db + " since sequence number " + since_number)
+
 request({url: db + '/_changes?feed=continuous&since=' + since_number })
   //.pipe(JSONStream.parse('results.*'))
   .pipe(es.split()) // by line
   .pipe(es.mapSync((line) => JSON.parse(line)))
-  .pipe(es.mapSync((data) => data.id ))
-  .pipe(es.through(function write(id) {
+  .pipe(es.through(function write(record) {
+    var seq = record.seq
+    var id = record.id
     curr_in_flight_reqs += 1
     if (curr_in_flight_reqs >= max_in_flight_reqs)
       this.pause()
@@ -42,6 +51,7 @@ request({url: db + '/_changes?feed=continuous&since=' + since_number })
           es_client.bulk({ body: bulk_body }, (err, resp) => { 
              if (err)
                console.log(err)
+             fs.writeFile(".couchdb_seq", seq)
              is_bulk_in_flight = false
              if (this.paused)
                this.resume()
