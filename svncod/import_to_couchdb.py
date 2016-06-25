3741620df3005c2059820b20b505a866cfeb5fbf
Bojie Li <bojieli@gmail.com>
2016-06-26 03:48:19 +0800

    import_to_couchdb second param indicate dbname

--- svncod/import_to_couchdb.py
+++ svncod/import_to_couchdb.py
@@ -7,9 +7,10 @@ sys.path.insert(0, './couchdb-python/')
 import couchdb
 
 walk_dir = sys.argv[1]
+dbname = sys.argv[2]
 
 server = couchdb.Server(url='http://localhost:5984/')
-db = server['moldb']
+db = server[dbname]
 
 start_time = time.time()
 file_count = 0
